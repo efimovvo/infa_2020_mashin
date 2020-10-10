4cec5fa149bea0508aaa9e1d0b579f3175d1d101
Efimov Viacheslav <efimovvo@yandex.ru>
2020-10-10 09:31:33 +0300

    Delete some repeated structures

--- Exc3.py
+++ Exc3.py
@@ -5,12 +5,12 @@ from numpy import pi
 def draw_window(x, y, width, height, frame_color, pane_color):
 	''' Function draws a WINDOW
 	
-	Attrubute:
+	Attrubutes:
 	-----------------------------------------------
 		x : float
-			horizontal coordinate of starting point
+			horizontal coordinate of window center point
 		y : float
-			vertical coordinate of starting point
+			vertical coordinate of window top point
 		width : float
 			horizontal size of the window
 		height : float
@@ -25,13 +25,22 @@ def draw_window(x, y, width, height, frame_color, pane_color):
 	pane_height = (0.5 - 1.5 * frame_width_ratio) * height
 
 	# Window frame
-	rect(screen, frame_color, (x, y, width, height))
+	rect(
+		screen,
+		frame_color,
+		(
+			x - 0.5 * width,
+			y,
+			width,
+			height
+		)
+		)
 	# Left top pane
 	rect(
 		screen,
 		pane_color,
 		(
-			x + frame_width_ratio * width,
+			x + (-0.5 + frame_width_ratio) * width,
 			y + frame_width_ratio * height,
 			pane_width,
 			pane_height,
@@ -42,7 +51,7 @@ def draw_window(x, y, width, height, frame_color, pane_color):
 		screen,
 		pane_color,
 		(
-			x + (0.5 + 0.5 * frame_width_ratio) * width,
+			x + 0.5 * frame_width_ratio * width,
 			y + frame_width_ratio * height,
 			pane_width,
 			pane_height
@@ -53,7 +62,7 @@ def draw_window(x, y, width, height, frame_color, pane_color):
 		screen,
 		pane_color,
 		(
-			x + frame_width_ratio * width,
+			x + (-0.5 + frame_width_ratio) * width,
 			y + (0.5 + 0.5 * frame_width_ratio) * height,
 			pane_width,
 			pane_height
@@ -64,7 +73,7 @@ def draw_window(x, y, width, height, frame_color, pane_color):
 		screen,
 		pane_color,
 		(
-			x + (0.5 + 0.5 * frame_width_ratio) * width,
+			x + 0.5 * frame_width_ratio * width,
 			y + (0.5 + 0.5 * frame_width_ratio) * height,
 			pane_width,
 			pane_height
@@ -74,7 +83,7 @@ def draw_window(x, y, width, height, frame_color, pane_color):
 def draw_cat(x, y, width, height, hair_color, eyes_color):
 	''' Function draws a CAT
 	
-	Attrubute:
+	Attrubutes:
 	-----------------------------------------------
 		x : float
 			cat horizontal position
@@ -90,7 +99,7 @@ def draw_cat(x, y, width, height, hair_color, eyes_color):
 			color of cat's eyes
 	'''
 
-# Body
+# Cat's body
 	ellipse(screen, hair_color, (x + width/3, y - height/6, width/1.5, height/2.5))
 	ellipse(screen, black, (x + width/3, y - height/6, width/1.5, height/2.5), 1)
 
@@ -111,7 +120,7 @@ def draw_cat(x, y, width, height, hair_color, eyes_color):
 
 	ellipse(screen, hair_color, (x + width/2.5, y + height/3, width/7, height/1.7))
 	ellipse(screen, black, (x + width/2.5, y + height/3, width/7, height/1.7), 1)
-# Eyes
+# Cat's eyes
 	ellipse(screen, eyes_color, (x-12*width/24, y-height/4.9, width/10, height/5))
 	ellipse(screen, black, (x-12*width/24, y-height/4.9, width/10, height/5), 1)
 
@@ -120,99 +129,129 @@ def draw_cat(x, y, width, height, hair_color, eyes_color):
 
 	ellipse(screen, black, (x-11*width/24, y-height/5.3, width/30, height/8))
 	ellipse(screen, black, (x-14*width/24, y-height/5.3, width/30, height/8))
-# Ear
-	polygon(screen, pink, [(x-width/1.6,y-height/2.5), (x-width/1.8,y-height/2.9),
-		                               (x-width/1.61,y-height/4)])
-	polygon(screen, hair_color, [(x-width/1.6,y-height/2.5), (x-width/1.8,y-height/2.9),
-		                               (x-width/1.61,y-height/4)],3)                               
-	polygon(screen, black, [(x-width/1.6,y-height/2.5), (x-width/1.8,y-height/2.9),
-		                               (x-width/1.61,y-height/4)], 1)
+# Cat's ear
+	polygon(screen, pink, [(x-width/1.6, y-height/2.5), (x-width/1.8, y-height/2.9),
+										(x-width/1.61, y-height/4)])
+	polygon(screen, hair_color, [(x-width/1.6, y-height/2.5), (x-width/1.8, y-height/2.9),
+										(x-width/1.61, y-height/4)], 3)                               
+	polygon(screen, black, [(x-width/1.6, y-height/2.5), (x-width/1.8, y-height/2.9),
+										(x-width/1.61, y-height/4)], 1)
 
-	polygon(screen, pink, [(x-width/1.6+width/4,y-height/2.5), (x-width/1.75+width/8,y-height/2.9),
-		                               (x-width/1.61+width/4,y-height/4)])
-	polygon(screen, hair_color, [(x-width/1.6+width/4,y-height/2.5), (x-width/1.75+width/8,y-height/2.9),
-		                               (x-width/1.61+width/4,y-height/4)],3)                               
-	polygon(screen, black, [(x-width/1.6+width/4,y-height/2.5), (x-width/1.75+width/8,y-height/2.9),
-		                               (x-width/1.61+width/4,y-height/4)], 1)
-# Nose
+	polygon(screen, pink, [(x-width/1.6+width/4, y-height/2.5), (x-width/1.75+width/8, y-height/2.9),
+										(x-width/1.61+width/4, y-height/4)])
+	polygon(screen, hair_color, [(x-width/1.6+width/4, y-height/2.5), (x-width/1.75+width/8, y-height/2.9),
+										(x-width/1.61+width/4, y-height/4)],3)                               
+	polygon(screen, black, [(x-width/1.6+width/4, y-height/2.5), (x-width/1.75+width/8, y-height/2.9),
+										(x-width/1.61+width/4, y-height/4)], 1)
+# Cat's nose
 	polygon(screen, black, [(x-width/1.89, y+height/30), (x-width/2.04, y+height/30),
-	                               (x-width/1.98, y+height/15)])
-	pygame.draw.line(screen, black, (x-width/1.98, y+height/15), (x-width/1.98, y+height/10), 1)
-	pygame.draw.arc(screen, black, (x-width/1.98, y+height/12, width/20, height/20), pi, pi+pi, 1)
-	pygame.draw.arc(screen, black, (x-width/1.98-width/20, y+height/12, width/20, height/20), pi, pi+pi, 1)
+										(x-width/1.98, y+height/15)])
+	line(screen, black, (x-width/1.98, y+height/15), (x-width/1.98, y+height/10), 1)
+	arc(screen, black, (x-width/1.98, y+height/12, width/20, height/20), pi, 2*pi, 1)
+	arc(screen, black, (x-width/1.98-width/20, y+height/12, width/20, height/20), pi, 2*pi, 1)
 
-	pygame.draw.arc(screen, black, (x-width/2.1, y+height/17, width/6, height/15),0, ((pi+pi+pi+pi+pi)/6), 1)
-	pygame.draw.arc(screen, black, (x-width/2.1, y+height/11, width/6, height/15),0, ((pi+pi+pi+pi+pi)/6), 1)
-	pygame.draw.arc(screen, black, (x-width/2.1, y+height/7, width/6, height/15),0, ((pi+pi+pi+pi+pi)/6), 1)
-	pygame.draw.arc(screen, black, (x-width/1.4, y+height/17, width/6, height/15), pi/6,pi, 1)
-	pygame.draw.arc(screen, black, (x-width/1.4, y+height/11, width/6, height/15), pi/6,pi, 1)
-	pygame.draw.arc(screen, black, (x-width/1.4, y+height/7, width/6, height/15), pi/6,pi, 1)
+	arc(screen, black, (x-width/2.1, y+height/17, width/6, height/15),0, (pi * 5/6), 1)
+	arc(screen, black, (x-width/2.1, y+height/11, width/6, height/15),0, (pi * 5/6), 1)
+	arc(screen, black, (x-width/2.1, y+height/7, width/6, height/15),0, (pi * 5/6), 1)
+	arc(screen, black, (x-width/1.4, y+height/17, width/6, height/15), pi/6,pi, 1)
+	arc(screen, black, (x-width/1.4, y+height/11, width/6, height/15), pi/6,pi, 1)
+	arc(screen, black, (x-width/1.4, y+height/7, width/6, height/15), pi/6,pi, 1)
 
-# Klubok
-def draw_ball(x, y, width, height):
-	ellipse(screen, gray, (x, y, width, width))
-	ellipse(screen, black, (x, y, width, width), 2)
-	pygame.draw.arc(screen, black, (x+width/8,y+ width/4, width/2, width/3), pi/6,pi, 1)
-	pygame.draw.arc(screen, black, (x+width/8,y+ width/2.5, width/2, width/3), pi/6,pi, 1)
-	pygame.draw.arc(screen, black, (x+width/8,y+ width/2, width/2, width/3), pi/6,pi, 1)
+# Ball
+def draw_ball(x, y, width, height, ball_color, fiber_color):
+	''' Function draws a BALL
+	
+	Attrubutes:
+	-----------------------------------------------
+		x : float
+			ball horizontal position
+		y : float
+			ball vertical position
+		width : float
+			ball size in horizontal
+		height : float
+			ball size in vertical
+		ball_color : tuple of 3 int
+			color of ball
+		fiber_color : tuple of 3 int
+			color of ball border and fiber lines
+	'''
+	ellipse(screen, ball_color, (x, y, width, width))
+	ellipse(screen, fiber_color, (x, y, width, width), 2)
 
-	pygame.draw.arc(screen, black, (x+width/4,y+ width/3, width/2, width/3), pi+pi/4,pi+pi, 1)
-	pygame.draw.arc(screen, black, (x+width/4,y+ width/2.5, width/2, width/3), pi+pi/4,pi+pi, 1)
-	pygame.draw.arc(screen, black, (x+width/4,y+ width/2, width/2, width/3), pi+pi/4,pi+pi, 1)
+	# Top 3 arcs of on the ball from the top to the bottom
+	pygame.draw.arc(screen, fiber_color, (x + width/8, y + width/4, width/2, width/3), pi/6,pi, 1)
+	pygame.draw.arc(screen, fiber_color, (x + width/8, y + width/2.5, width/2, width/3), pi/6,pi, 1)
+	pygame.draw.arc(screen, fiber_color, (x + width/8, y + width/2, width/2, width/3), pi/6,pi, 1)
 
-	pygame.draw.arc(screen, black, (x-1.5*width,y+ 0.84*width, 2*width, width/2), pi/6,pi, 1)
+	# Bottom 3 arcs of on the ball from the top to the bottom
+	pygame.draw.arc(screen, fiber_color, (x + width/4, y + width/3, width/2, width/3), pi + pi/4, pi+pi, 1)
+	pygame.draw.arc(screen, fiber_color, (x + width/4, y + width/2.5, width/2, width/3), pi + pi/4, pi+pi, 1)
+	pygame.draw.arc(screen, fiber_color, (x + width/4, y + width/2, width/2, width/3), pi + pi/4, pi+pi, 1)
 
+	# One external fiber
+	pygame.draw.arc(screen, fiber_color, (x - 1.5*width, y + 0.84*width, 2*width, width/2), pi/6, pi, 1)
 
 pygame.init()
 
-white = ((255,255,255))
-blue = ((0,0,255))
-green = ((0,255,0))
-red = ((255,0,0))
-black = ((0,0,0))
-orange = ((255,100,10))
-yellow = ((255,255,0))
-blue_green = ((0,255,170))
-marroon = ((115,0,0))
-lime = ((180,255,100))
-pink = ((255,100,180))
-purple = ((240,0,255))
-gray = ((127,127,127))
-magenta = ((255,0,230))
-brown = ((100,40,0))
-forest_green = ((0,50,0))
-navy_blue = ((0,0,100))
-rust = ((210,150,75))
-dandilion_yellow = ((255,200,0))
-highlighter = ((255,255,100))
-sky_blue = ((0,255,255))
-light_gray = ((200,200,200))
-dark_gray = ((50,50,50))
-tan = ((230,220,170))
-coffee_brown = ((200,190,140))
+white = (255,255,255)
+blue = (0,0,255)
+green = (0,255,0)
+red = (255,0,0)
+black = (0,0,0)
+orange = (255,100,10)
+yellow = (255,255,0)
+blue_green = (0,255,170)
+marroon = (115,0,0)
+lime = (180,255,100)
+pink = (255,100,180)
+purple = (240,0,255)
+gray = (127,127,127)
+magenta = (255,0,230)
+brown = (100,40,0)
+forest_green = (0,50,0)
+navy_blue = (0,0,100)
+rust = (210,150,75)
+dandilion_yellow = (255,200,0)
+highlighter = (255,255,100)
+sky_blue = (0,255,255)
+light_gray = (200,200,200)
+dark_gray = (50,50,50)
+tan = (230,220,170)
+coffee_brown = (200,190,140)
 
 FPS = 30
-screen = pygame.display.set_mode((600, 600))
+screen_width = 400
+screen_height = 600
+
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Background = the wall color
 screen.fill(brown)
 
-# Background
+# The floor
 rect(screen, rust, (0, 250, 600, 450))
 
 # Drawing the windows
-draw_window(60, 20, 120, 150, navy_blue, sky_blue)
-draw_window(230, 20, 120, 150, navy_blue, sky_blue)
-draw_window(400, 20, 120, 150, navy_blue, sky_blue)
+window_width = screen_width / 3
+window_height = screen_height / 4
+draw_window(screen_width / 12, 20, window_width, window_height, navy_blue, sky_blue)
+draw_window(screen_width / 2, 20, window_width, window_height, navy_blue, sky_blue)
+draw_window(screen_width * 11 / 12, 20, window_width, window_height, navy_blue, sky_blue)
 
 # Drawing the cats
-draw_cat(100, 300, 120, 60, orange, green)
-draw_cat(430, 300, 150, 75, gray, sky_blue)
-draw_cat(300, 460, 200, 100, brown, green)
+draw_cat(screen_width / 4, 300, screen_width / 3, 60, orange, green)
+draw_cat(screen_width * 7 / 8, 300, screen_width * 2 / 5, 75, gray, sky_blue)
+draw_cat(screen_width * 3 / 4, 460, screen_width / 2, 100, brown, green)
 
 # Drawing the balls
-draw_ball(120, 500, 80, 80)	
-draw_ball(50, 370, 70, 70)	
-draw_ball(240, 280, 65, 65)	
-draw_ball(500, 500, 90, 90)
+basic_ball_radius = screen_width / 40
+
+draw_ball(screen_width / 3, screen_height * 5 / 6, 8 * basic_ball_radius, 8 * basic_ball_radius, gray, black)	
+draw_ball(screen_width / 8, screen_height * 7 / 12, 7 * basic_ball_radius, 7 * basic_ball_radius, gray, white)
+draw_ball(screen_width * 2 / 3, screen_height * 4 / 7, 6  * basic_ball_radius, 6 * basic_ball_radius, gray, black)
+draw_ball(screen_width * 11 / 12, screen_height * 5 / 6, 9 * basic_ball_radius, 9 * basic_ball_radius, gray, black)
 
 pygame.display.update()
 clock = pygame.time.Clock()
